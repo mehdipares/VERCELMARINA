5997072d7bde7803336ca723e1faea0e1890d376
Dims <mehdipares@85gmail.com>
2025-02-19 13:05:33 +0100

    /add

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const service = require('../services/users');
 //la route pour lire les infos d'un utilisateur
 router.get('/:id', service.getById);
 //la route pour ajouter un utilisateur
-router.put('add', service.add);
+router.put('/add', service.add);
 //la route pour modifier un utilisateur
 router.patch('/:id', service.update);
 //la route pour supprimer un utilisateur
