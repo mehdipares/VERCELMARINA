c77f0dadbc2dd6e598dec43005f03de2846ffad8
Dims <mehdipares@85gmail.com>
2025-02-19 14:35:06 +0100

    authenticate

--- services/users.js
+++ services/users.js
@@ -1,5 +1,8 @@
 
 const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 
 // Récupérer un utilisateur par ID
 exports.getById = async (req, res, next) => {
@@ -76,6 +79,43 @@ exports.delete = async (req, res, next) => {
         await User.deleteOne({_id: id });
 
         return res.status(204).json('delete_ok');
+    } catch (error) {
+        return res.status(501).json(error);
+    }
+}
+
+exports.authenticate = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email: email }, '-__v -createdAt -updateAt');
+        if (user) {
+            bcrypt.compare(password, user.password, function(err, response) {
+                if(err) {
+                    throw new Error(err);
+                }
+                if (response) {
+                    delete user._doc.password;
+                    
+                    const expireIn = 24 * 60 * 60;
+                    const token    = jwt.sign({
+                        user: user
+                    },
+                    SECRET_KEY,
+                    {
+                        expiresIn: expireIn
+                    });
+
+                    res.header('Authorization', 'bearer' + token);
+            
+                    return res.status(200).json('authenticate_succeed');
+                    }
+
+                    return res.status(403).json('wrong_credentials');
+            });
+        } else {
+            return res.status(404).json('user_not_found');
+        }
     } catch (error) {
         return res.status(501).json(error);
     }
