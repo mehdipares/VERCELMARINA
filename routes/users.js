55e0c8e585d5fcaedbe8e74e0da58c6a3cfb072a
Dims <mehdipares@85gmail.com>
2025-02-20 14:54:28 +0100

    privatisation route

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,16 @@ const express = require ('express');
 const router = express.Router();
 
 const service = require('../services/users');
+const private = require('../middlewares/private');
 
 //la route pour lire les infos d'un utilisateur
-router.get('/:id', service.getById);
+router.get('/:id', private.checkJWT, service.getById);
 //la route pour ajouter un utilisateur
 router.put('/add', service.add);
 //la route pour modifier un utilisateur
-router.patch('/:id', service.update);
+router.patch('/:id', private.checkJWT, service.update);
 //la route pour supprimer un utilisateur
-router.delete('/:id', service.delete);
+router.delete('/:id', private.checkJWT, service.delete);
 
 //route authentification
 router.post ('/authenticate', service.authenticate);
