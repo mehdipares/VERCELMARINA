99c8483071b52d4babe421ac04892d27e2797d58
Dims <mehdipares@85gmail.com>
2025-02-20 15:02:38 +0100

    private routes v2

--- middlewares/private.js
+++ middlewares/private.js
@@ -2,8 +2,8 @@ const jwt    = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
 
 exports.checkJWT = async (req, res, next) => {
-    let token = req.headers['x-acess-token'] || req.headers['authorization'];
-    if (!!token && token.startsWith('bearer ')) {
+    let token = req.headers['x-access-token'] || req.headers['authorization'];
+    if (!!token && token.startsWith('Bearer ')) {
         token = token.slice(7, token.length);
     }
 
